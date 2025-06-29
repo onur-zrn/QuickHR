d24b8da6810dd8b852e46aa87d95cb5f5d064680
Onur Zeren <onurzrn@gmail.com>
2025-06-29 13:15:30 +0300

    Security ayarları güncellendi.

--- src/main/java/com/quickhr/config/SecurityConfig.java
+++ src/main/java/com/quickhr/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
                             .permitAll()
                               .requestMatchers(ADMIN + "/**").hasAnyAuthority(EAdminRole.ADMIN.toString(), EAdminRole.SUPER_ADMIN.toString())
                               .requestMatchers(COMPANY + "/**").hasAuthority(EUserRole.MANAGER.toString())
-                              .requestMatchers(EMPLOYEE + "/**").hasAuthority(EUserRole.PERSONAL.toString())
+                              .requestMatchers(EMPLOYEE + "/**").hasAnyAuthority(EUserRole.MANAGER.toString(),EUserRole.PERSONAL.toString())
                               .requestMatchers(USER + "/**").hasAnyAuthority(EUserRole.MANAGER.toString(),EUserRole.PERSONAL.toString())
                             .anyRequest().permitAll();
                 });
